9cccb138fef0869b3f0154cf01ed0d291a752adf
Einsuomi <97128113+Einsuomi@users.noreply.github.com>
2023-07-13 23:44:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,9 +59,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    # streamlit.dataframe(fruityvice_normalized)
+    data_from_fruityvice = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(data_from_fruityvice)
 except URLError as e:
   streamlit.error()
 
