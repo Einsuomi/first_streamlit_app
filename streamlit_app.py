1bbd5ca0f256aa17326391e84a69e33f943bb7b8
Einsuomi <97128113+Einsuomi@users.noreply.github.com>
2023-07-13 14:00:18 +0300

    Update streamlit_app.py
    add interaction with users' input

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,16 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # show the data
 streamlit.dataframe(fruityvice_normalized)
+
+
+# new section to display fruityvice API response
+
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+
